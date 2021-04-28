9b1a67ad57c7c1528130180366320a962067203a
foysal11203004 <foysal11203004@gmail.com>
2021-04-28 17:20:31 +0600

    check booked or not

--- app/Http/Controllers/Frontend/BookingController.php
+++ app/Http/Controllers/Frontend/BookingController.php
@@ -11,25 +11,45 @@ class BookingController extends Controller
 {
     public function showCar($id)
     {
-        $car=Product::find($id);
-        return view('frontend.layouts.car-show',compact('car'));
+        $car = Product::find($id);
+        return view('frontend.layouts.car-show', compact('car'));
     }
 
     public function booking(Request $request)
     {
-        $car=Product::find($request->car_id);
-        $daysCalculate=strtotime($request->to_date)-strtotime($request->from_date);
-        $daysCalculate=round($daysCalculate / (60 * 60 * 24));
-            Booking::create([
-               'car_id'=>$request->car_id,
-                'user_id'=>auth()->user()->id,
-                'booking_from'=>$request->from_date,
-                'booking_to'=>$request->to_date,
-                'details'=>$request->details,
-                'rate'=>$car->price,
-                'total'=>$car->price*$daysCalculate,
-            ]);
+        $car = Product::find($request->car_id);
+        $daysCalculate = strtotime($request->to_date) - strtotime($request->from_date);
+        $daysCalculate = round($daysCalculate / (60 * 60 * 24));
+        //need to check the room/ the flat/ car/ bike is available to booked or not
+        $fromDate = date("Y-m-d", strtotime($request->from_date));
+        $toDate = date("Y-m-d", strtotime($request->to_date));
 
-            return redirect()->back()->with('message','Booking created Successfully');
+        $checkBook = Booking::query()->where('car_id', $request->car_id);
+        //            ->where('user_id',auth()->user()->id) // for this user
+//                    ->where('slot_id',$request->slot_id) //for specific slot
+
+        $checkBook->where(function ($query) use ($fromDate, $toDate) {
+            $query->whereBetween('booking_from', [$fromDate, $toDate])
+                ->orWhereBetween('booking_to', [$fromDate, $toDate]);
+        });
+        $checkBook = $checkBook->get();
+
+        if ($checkBook->count() == 0) {
+            //if available then create booking/reservation
+            Booking::create([
+                'car_id' => $request->car_id,
+                'user_id' => auth()->user()->id,
+                'booking_from' => $request->from_date,
+                'booking_to' => $request->to_date,
+                'details' => $request->details,
+                'rate' => $car->price,
+                'total' => $car->price * $daysCalculate,
+            ]);
+            return redirect()->back()->with('message', 'Booking created Successfully');
+        } else {
+            return redirect()->back()->with('message', 'Already booked.');
+        }
+
+
     }
 }
